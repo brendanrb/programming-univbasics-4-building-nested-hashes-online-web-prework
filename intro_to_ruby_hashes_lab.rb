8c9e64ba4b6db16a2967887fb25648f16c0aa6fd
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-22 12:56:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,8 +16,11 @@ end
 
 def monopoly_with_third_tier
 	{:railroads =>
-	   {:pieces => 4}
-	   {:rent_in_dollars => }
+	   {:pieces => 4
+	     {:rent_in_dollars => }
+	   }
+	   }
+	   
 	  
 	}
 	  
