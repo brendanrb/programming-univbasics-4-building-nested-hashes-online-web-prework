c6c4f31d4ee5d1abf750df4f42c9ce7e275b7a41
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-22 13:18:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,13 +51,17 @@ def monopoly_with_fourth_tier
 	   :four_pieces_owned => 200
 	   },
 	   :names =>
-	   {:reading_railroad =>{"mortgage value"= "100"},
+	   {:reading_railroad =>
+	   {"mortgage value"= "100"},
       
-      :pennsylvania_railroad =>{"mortgage value"= "100"},
+      :pennsylvania_railroad =>
+      {"mortgage value"= "200"},
       
-      :b_and_o_railroad =>{"mortgage value"= "100"},
+      :b_and_o_railroad =>
+      {"mortgage value"= "300"},
     
-      :shortline_railroad =>{"mortgage value"= "100"},
+      :shortline_railroad =>
+      {"mortgage value"= "400"},
 	   }
 	}
 	}
