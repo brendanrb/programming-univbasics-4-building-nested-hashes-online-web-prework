8772abc417ea3f9236578547f7e7041075b3e577
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-22 12:53:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	{:railroads => {:rent_in_dollars => }
-	   {:pieces => 4},
+	{:railroads => {:rent_in_dollars => },
+	   {:pieces => 4}
 	  
 	}
 	  
